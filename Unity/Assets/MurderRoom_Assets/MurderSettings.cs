99ab7749411b3fb62bac4b6bd58a87fc9c39e1c7
Airwaffle <simonanderssonpro@gmail.com>
2015-02-07 22:37:45 +0100

    ssafsdsad

---
+++ Unity/Assets/MurderRoom_Assets/MurderSettings.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct MurderData
+{
+	public int CheatingLevel;
+	public int MurderLevel;
+}
+
+[System.Serializable]
+public struct BorderSettings
+{
+	public int CheatingNeededForSuspicion;
+	public int MurderNeededForSuspicion;
+}
+
+public class MurderSettings : ScriptableObject
+{
+	public MurderData DummyData;
+	public BorderSettings BorderData;
+	
+	public bool SuspectsNothing()
+	{
+		return DummyData.MurderLevel < BorderData.MurderNeededForSuspicion && DummyData.CheatingLevel < BorderData.CheatingNeededForSuspicion;
+	}
+	
+	public bool SuspectsCheating()
+	{
+		return DummyData.MurderLevel < BorderData.MurderNeededForSuspicion && DummyData.CheatingLevel >= BorderData.CheatingNeededForSuspicion;
+	}
+	
+	public bool SuspectsMurder()
+	{
+		return DummyData.MurderLevel >= BorderData.MurderNeededForSuspicion;
+	}
+}
