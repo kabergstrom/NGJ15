18ef244d675db74f1683910479b3d57130c9c62c
Karl Bergström <karl.anton.bergstrom@gmail.com>
2015-02-07 20:35:57 +0100

    Text fade settings!

--- Unity/Assets/Editor/TextFadeInspector.cs
+++ Unity/Assets/Editor/TextFadeInspector.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEditor;
 
 
-[CustomEditor(typeof(TextFade))]
+//[CustomEditor(typeof(TextFade))]
 public class TextFadeInspector : Editor
 {
     TextFade Fade { get { return target as TextFade; }}
