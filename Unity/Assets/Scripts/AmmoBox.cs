c80a93aadf5a692e98bb60740e6eb8f85ff088d2
Tobias Johansson <Mockarutan@gmail.com>
2015-02-08 00:34:20 +0100

    no message

---
+++ Unity/Assets/Scripts/AmmoBox.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoBox : MonoBehaviour
+{
+    [SerializeField]
+    private int NormalGunAmmo;
+    [SerializeField]
+    private int BetterGunAmmo;
+    [SerializeField]
+    private int GranadeAmmo;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag.Equals("Player"))
+        {
+            WeaponSystem weapons = collider.GetComponent<WeaponSystem>();
+            weapons.NormalGunAmmo += NormalGunAmmo;
+            weapons.BetterGunAmmo += BetterGunAmmo;
+            weapons.GranadeCount += GranadeAmmo;
+            Destroy(gameObject);
+        }
+    }
+}
