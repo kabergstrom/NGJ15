6ef8c31e466dd193375be150298893e388f4a5b1
Airwaffle <simonanderssonpro@gmail.com>
2015-02-08 13:24:32 +0100

    Debug keys.

---
+++ Unity/Assets/MurderRoom_Assets/DebugInput.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugInput : MonoBehaviour {
+
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.Q))
+		{
+			var lights = FindObjectsOfType(typeof(Light)) as Light[];
+			 foreach(Light light in lights)
+			 {
+				 light.intensity += 0.15f * Time.deltaTime;
+			 }			
+		}
+		else if (Input.GetKey(KeyCode.W))
+		{
+			var lights = FindObjectsOfType(typeof(Light)) as Light[];
+			 foreach(Light light in lights)
+			 {
+				 light.intensity -= 0.15f* Time.deltaTime;
+			 }			
+		}
+
+		if (Input.GetKey(KeyCode.Alpha1))
+		{
+			MurderState.Instance.Data.CheatingLevel = 0;
+			MurderState.Instance.Data.MurderLevel = 0;
+			MurderState.Instance.Data.CoatGone = false;
+			ColorAdjuster.Instance.UpdateColors();
+		}
+		if (Input.GetKey(KeyCode.Alpha2))
+		{
+			MurderState.Instance.Data.CheatingLevel = 2;
+			MurderState.Instance.Data.MurderLevel = 0;
+			MurderState.Instance.Data.CoatGone = false;
+			ColorAdjuster.Instance.UpdateColors();
+		}
+		if (Input.GetKey(KeyCode.Alpha3))
+		{
+			MurderState.Instance.Data.CheatingLevel = 2;
+			MurderState.Instance.Data.MurderLevel = 2;
+			MurderState.Instance.Data.CoatGone = false;
+			ColorAdjuster.Instance.UpdateColors();
+		}
+	}
+}
