62aafbcbf84d8b1fe9c1600690dd1dd74fd94727
Tobias Johansson <Mockarutan@gmail.com>
2015-02-08 05:19:34 +0100

    no message

--- Unity/Assets/Scripts/GameObjectActivator.cs
+++ Unity/Assets/Scripts/GameObjectActivator.cs
@@ -6,10 +6,14 @@ public class GameObjectActivator : Trigger
 {
     [SerializeField]
     private List<GameObject> GameObjects;
+    [SerializeField]
+    private List<GameObject> DeactivateGameObjects;
 
     public override void OnTrigger()
     {
         foreach (var obj in GameObjects)
             obj.SetActive(true);
+        foreach (var obj in DeactivateGameObjects)
+            obj.SetActive(false);
     }
 }
